f4d5b1a96042e82ddf84be9cee84512512bde0e8
Zhetian Dong <158158228+Xiaotian34@users.noreply.github.com>
2025-04-27 12:32:29 +0200

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'aspose/cells' => array(
+            'pretty_version' => '25.4',
+            'version' => '25.4.0.0',
+            'reference' => '04a15c714cda0e04b969e81b7bb1a42d939c163c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../aspose/cells',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
