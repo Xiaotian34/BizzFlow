b2bbecb195e429788006641150dba42bb060475a
JoseAntonioHH <ocococ26@gmail.com>
2025-04-27 14:14:18 +0200

    add menu desplegable

--- view/menu_view.php
+++ view/menu_view.php
@@ -2,11 +2,8 @@
 if (!isset($_SESSION["correo"])) {
     echo '
         <header>
-            <h1>Galeria</h1>
+            <h1 href="index.php>Aqui irá el logo</h1>
             <ul>
-                <div>
-                    <a href="index.php">Inicio</a>
-                </div>
                 <div>
                     <a href="index.php?controlador=usuarios&action=login">Iniciar Sesion</a>
                     <a href="index.php?controlador=usuarios&action=registro">Registrate</a>
@@ -17,16 +14,24 @@ if (!isset($_SESSION["correo"])) {
 } else {
     echo '
         <header>
-            <h1>Galeria</h1>
+            <h1>Aqui irá el logo
+                <a href="index.php"></a>
+            </h1>
             <ul>
-                <div>
-                    <a href="index.php">Inicio</a>
+                <div class="dropdown">
+                <button onclick="myFunction()" class="dropbtn">Dropdown</button>
+                    <div id="myDropdown" class="dropdown-content">
+                        <input type="text" placeholder="Search.." id="myInput" onkeyup="filterFunction()">
+                        <a href="index.php?controlador=contactar&action=home">Contactar</a>
+                        <a href="index.php?controlador=documentos&action=gestionarDocumentos">Gestionar documentos</a>
+                        <a href="index.php?controlador=usuarios&action=gestionarUsuarios">Gestionar usuarios</a>
+                    </div>
                 </div>
-                <div>
-                    <a href="index.php?controlador=contactar&action=home">Contactar</a>
-                    <a href="index.php?controlador=documentos&action=gestionarDocumentos">Gestionar documentos</a>
-                    <a href="index.php?controlador=usuarios&action=gestionarUsuarios">Gestionar usuarios</a>
-                    <a href="index.php?controlador=usuarios&action=logout">Cerrar sesion</a>
+                <div class="dropdown">
+                <button onclick="myFunction()" class="dropbtn">Aqui se veria el usuario</button>
+                    <div id="myDropdown" class="dropdown-content">
+                        <a href="index.php?controlador=usuarios&action=logout">Cerrar sesion</a>
+                    </div>
                 </div>
             </ul>
         </header>
